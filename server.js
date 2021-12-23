c1e8cb666a01dc2fe2aa92d0f6d9a2161c508334
Michael Li <thisislimichael@gmail.com>
2021-12-23 15:44:05 -0800

    functions to add employees and update roles

--- server.js
+++ server.js
@@ -44,6 +44,11 @@ function mainQuestions() {
           name: "Update an employee role",
           value: "updateEmployeeRole"
         },
+
+        {
+          name: "Exit",
+          value: "exitApp"
+        },
       ]
     },
     
@@ -76,6 +81,10 @@ function mainQuestions() {
 
     if (choice == "updateEmployeeRole") {
       updateEmployeeRole()
+    }
+
+    if (choice == "exitApp") {
+      exitApp()
     }
   })
 }
@@ -149,31 +158,83 @@ function addRole() {
 }
 
 function addEmployee() {
+  db.viewAllEmployees().then(([rows])=>{
+    let employeeNew = rows
+    const employeeList = employeeNew.map(({id,first_name,last_name,role_id, manager_id})=>({
+      first_name: first_name,
+      last_name: last_name,
+      role_id:role_id,
+      value:id,
+      manager_id: manager_id
+    }))
+    prompt([
+      {
+        type: "input",
+        name: "first_name",
+        message: "What is the employee's first name?"
+      },
 
+      {
+        type: "input",
+        name: "last_name",
+        message: "What is the employee's last name?"
+      },
+  
+      {
+        type: "list",
+        name: "role_id",
+        message: "What is the employee's role?",
+        choices:employeeList
+      },
+  
+      {
+        type: "list",
+        name: "manager_id",
+        message: "Who is the employee's manager?",
+        choices: employeeList
+      }
+    ]).then(res=>{
+      db.createEmployee(res).then(()=>mainQuestions())
+    })
+  })
 
 }
 
 function updateEmployeeRole() {
   db.viewAllEmployees().then(([rows])=>{
     let employees = rows
-    let employeesChoices = employees.map(({id,first_name,last_name})=>({
+    let employeesChoices = employees.map(({id,first_name,last_name, role_id, manager_id})=>({
       first_name: first_name,
       last_name: last_name,
-      value:id
+      role_id:role_id,
+      value:id,
+      manager_id: manager_id
     }))
     prompt([
       {
-        type: "list",
-        name: "title",
+        type: "rawlist",
+        name: "id",
         message: "Which employee's role do you want to update?",
+        choices: employeesChoices
+      },
+
+      {
+        type: "rawlist",
+        name: "role_id",
+        message: "What is the employee's new role?",
         choices: employeesChoices
       }
     ]).then(res=>{
-      let name = res
-      db.createDepartment(name).then(()=>mainQuestions())
+      let name = res.id
+      let role = res.role_id
+      db.updateEmployeeRole(name, role).then(()=>mainQuestions())
     })
   })
 
+}
+
+function exitApp() {
+  process.exit();
 }
 
 mainQuestions()
